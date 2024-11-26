afcec3fda06d2f3d08a129e442bb2767e24b93a1
Eduardo Santos <edukadudu18@gmail.com>
2024-11-25 22:46:14 -0300

    Ajustado parametros do Python

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Contador-Objetos-IA
 Utilizando dos conceitos, conhecimentos e ferramentas da disciplina de Redes Neurais, para desenvolver uma IA capaz de realizar a contagem e rastreamento de objetos, com auxílio de uma câmera.
+
+
+## Participação no desenvolvimento do sistema de contagem de estudantes:
+
+Contribuir na elaboração do código para análise de imagens e detecção de objetos, utilizando frameworks de IA e visão computacional como TensorFlow, PyTorch e OpenCV.
+
+## Estudo de redes neurais convolucionais e detecção de objetos:
+Aprimorar conhecimentos em redes neurais convolucionais (CNNs) e técnicas de detecção de objetos, como YOLO e Faster R-CNN, para aplicar no projeto.
+
+## Implementação de funcionalidades de detecção e contagem de objetos:
+Desenvolver e otimizar funcionalidades para identificar, contar e monitorar os objetos nas imagens, garantindo alta precisão e desempenho.
+
+## Pesquisa de técnicas de pré-processamento de imagens: 
+Investigar e aplicar métodos de aprimoramento de imagens, como normalização de cores, ajuste de brilho e contraste, para melhorar a qualidade do treinamento do modelo.
+
+
+
+
+
---
+++ contador_objetos.py
@@ -0,0 +1,45 @@
+import cv2  
+import numpy as np
+# escadaFacul
+video = cv2.VideoCapture('escalator.mp4')
+contador = 0
+liberado = False
+
+# Ajustar métricas relacionadas a escada
+
+while True:
+    ret,img = video.read()
+    img = cv2.resize(img,(1100,720),)
+    imgGray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+    x,y,w,h = 490,230,30,150
+    imgTh = cv2.adaptiveThreshold(imgGray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 12)
+    kernel = np.ones((8,8), np.uint8)
+    imgDil = cv2.dilate(imgTh,kernel,iterations=2)
+
+    recorte = imgDil[y:y+h,x:x+w]
+    brancos = cv2.countNonZero(recorte)
+
+    # variavel liberado para evitar nova contagem baseado no fps do video, impedir que a cada frame ele faça uma nova contagem
+    if brancos > 4000 and liberado == True:
+        contador +=1
+    if brancos < 4000:
+        liberado = True
+    else:
+        liberado =False
+
+    if liberado == False:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 4) # cor verde com alguem passando
+    else:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255, 0, 255),4) # cor rosa quando nao tem ninguem passando
+
+    cv2.rectangle(imgTh, (x, y), (x + w, y + h), (255, 255, 255), 6) # variavel com threshold para verificar se os pixels brancos estao corretos
+    
+    # Textos da imagem
+    cv2.putText(img,str(brancos),(x-30,y-50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),1) # contagem de pontos brancos
+    cv2.rectangle(img, (575,155), (575 + 88, 155 + 85), (255, 255, 255), -1) # quadrado branco para desenhar numero de pessoas contadas
+    cv2.putText(img, str(contador), (x+100, y), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 5) # numero de pessoas contadas
+
+    #print(contador)
+    cv2.imshow('video original',img)
+    cv2.imshow('video', cv2.resize(imgTh,(600,500)))
+    cv2.waitKey(20)
